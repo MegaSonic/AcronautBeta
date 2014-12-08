e90355924f6641b9bb1f86628a10695cd9a15ab3
Michael Biggs <eraserhead@gmail.com>
2014-12-08 12:10:50 -0700

    finished rabbit hat

---
+++ AcronautDemo/Assets/Scripts/RabbitHatAnimControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RabbitHatAnimControl : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = transform.parent.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		animator.SetTrigger ("Hide");
+	}
+
+	void OnTriggerExit2D(Collider2D coll){
+		animator.SetTrigger ("Show");
+	}
+
+}
