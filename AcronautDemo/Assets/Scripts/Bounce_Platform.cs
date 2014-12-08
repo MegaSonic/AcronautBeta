19c2271a8123c64bcf03d2484f1c15c6f6be8876
Michael Biggs <eraserhead@gmail.com>
2014-12-07 22:30:39 -0700

    Updated the trampoline with animation, and slightly tweaked code

--- AcronautDemo/Assets/Scripts/Bounce_Platform.cs
+++ AcronautDemo/Assets/Scripts/Bounce_Platform.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class Bounce_Platform : MonoBehaviour {
 
 	private PlayerController pc;
+	private SpriteRenderer sprite;
+	private Animator animator;
 
 	public float bounceMultiplier;
+	public float minBounce;
 	
 	// Use this for initialization
 	void Start () {
 		pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+		animator = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -22,5 +26,8 @@ public class Bounce_Platform : MonoBehaviour {
 		pc.RefreshAirMoves();
 		pc.gravityVelocity = 0f; // reset gravity, to get a little extra bounce
 		pc.vertVelocity *= bounceMultiplier * -1;
+		if (pc.vertVelocity < minBounce)
+			pc.vertVelocity = minBounce;
+		animator.SetTrigger ("Bounce");
 	}
 }
