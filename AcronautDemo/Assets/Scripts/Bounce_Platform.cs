e981711221054eb8bc249b18f22e1fd1438c5626
Michael Biggs <eraserhead@gmail.com>
2014-12-08 23:55:57 -0700

    put in some trampoline and bumper sound fx

--- AcronautDemo/Assets/Scripts/Bounce_Platform.cs
+++ AcronautDemo/Assets/Scripts/Bounce_Platform.cs
@@ -23,6 +23,13 @@ public class Bounce_Platform : MonoBehaviour {
 
 	// Reverse the player's vertical velocity, and multiply it by the bounce multiplier
 	void OnCollisionEnter2D(Collision2D coll){
+
+		SoundManager sm = coll.gameObject.transform.FindChild ("Sound Manager").GetComponent<SoundManager> ();
+		sm.play (SoundManager.TRAMPOLINE); // play player's sound
+
+		SoundManager sm2 = transform.FindChild ("Sound Manager").GetComponent<SoundManager> ();
+		sm2.play (SoundManager.TRAMPOLINE); // play trampoline's sound
+
 		pc.RefreshAirMoves();
 		pc.gravityVelocity = 0f; // reset gravity, to get a little extra bounce
 		pc.vertVelocity *= bounceMultiplier * -1;
