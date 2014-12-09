b8a15ed5cb6bc978c1c7742448bfcf7acf99d33a
Adam Kay <adamkay@backcode.com>
2014-12-09 16:11:03 -0700

    Added a Soft Reset button - hold R in any scene (only works when starting the game from the Main Menu) and other changes

---
+++ AcronautDemo/Assets/DevCommand.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevCommand : MonoBehaviour {
+	
+	public static DevCommand instance = null;
+
+	private float restartTimer = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		}
+		else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.R)) {
+			restartTimer -= Time.deltaTime;
+			if (restartTimer < 0f) {
+				foreach (GameObject o in Object.FindObjectsOfType<GameObject>()) {
+					Destroy(o);
+				}
+				Application.LoadLevel("intro");
+			}
+		}
+	}
+}
