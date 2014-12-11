53837a30a959108f7839ff8681daa83134ad7d66
Adam Kay <adamkay@backcode.com>
2014-12-10 22:59:43 -0700

    Found the code for scaling the screen

--- AcronautDemo/Assets/Scripts/ScreenSettings.cs
+++ AcronautDemo/Assets/Scripts/ScreenSettings.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class ScreenSettings : MonoBehaviour {
 
+	private float baseAspect = 640.0f / 360.0f;
+
 	// Use this for initialization
 	void Start () {
-		Screen.SetResolution(640, 360, false, 60);
+		float currAspect = 1.0f * Screen.width / Screen.height;
+
+		Camera.main.projectionMatrix = Matrix4x4.Scale(new Vector3(currAspect / baseAspect, 1.0f, 1.0f)) * Camera.main.projectionMatrix;
 
 	}
 	
